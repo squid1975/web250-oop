d9d1097d4ec33affc09ea307e513af0eedc25fd7
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-01 21:27:29 -0400

    Set (temp) visibility for all existing properties and methods in bicycle class

--- asgn02-inheritance/challenge-03-oac.php
+++ asgn02-inheritance/challenge-03-oac.php
@@ -2,21 +2,21 @@
 
 class Bicycle {
 
-  var $brand;
-  var $model;
-  var $year;
-  var $description = 'Used bicycle';
-  var $weight_kg = 0.0;
+  public $brand;
+  public $model;
+  public $year;
+  public $description = 'Used bicycle';
+  public $weight_kg = 0.0;
 
-  function name() {
+  public function name() {
     return $this->brand . " " . $this->model . " (" . $this->year . ")";
   }
 
-  function weight_lbs() {
+  public function weight_lbs() {
     return floatval($this->weight_kg) * 2.2046226218;
   }
 
-  function set_weight_lbs($value) {
+  public function set_weight_lbs($value) {
     $this->weight_kg = floatval($value) / 2.2046226218;
   }
 
