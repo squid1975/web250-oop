9ba6dad261f8907e94858dbb6a205c561a1522a4
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-11 13:44:33 -0400

    Complete constructor-arguments.php

--- asgn04-constructors/constructor-arguments.php
+++ asgn04-constructors/constructor-arguments.php
@@ -13,4 +13,11 @@ class Bird {
 $acadianFlycatcher = new Bird(['commonName'=>'Acadian Flycatcher', 'latinName' => 'Empidonax virescens']);
 
 $carolinaWren = new Bird(['commonName'=>'Carolina Wren', 'latinName' => 'Thryothorus ludovicianus']);
+
+echo "Common Name: " . $acadianFlycatcher->commonName ."<br>";
+echo "Latin Name: " . $acadianFlycatcher->latinName ."<br>";
+echo "<hr>";
+echo "Common Name: " . $carolinaWren->commonName ."<br>";
+echo "Latin Name: " . $carolinaWren->latinName ."<br>";
+
 ?>
