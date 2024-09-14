3c194c887ef59f9f45045a96a0833eeac9716bc8
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-13 23:25:17 -0400

    Create index for static-challenge

---
+++ static-challenge/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Web 250: Static Challenge</title>
+  </head>
+
+  <body>
+    <header>
+      <h1>Static Challenge</h1>
+      <nav role="navigation">
+        <ul>
+          <li><a href="challenge04.php">Challenge 04</a></li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
