71e0504c425ec3dede94784e93748c83aec9c56a
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-13 23:14:47 -0400

    Create index pages for asgn01 folders

---
+++ asgn01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Web 250: OOP Home</title>
+  </head>
+
+  <body>
+    <header>
+      <h1>Asgn 01: Bike and Bird Challenge</h1>
+      <nav role="navigation">
+        <ul>
+          <li><a href="bike-challenge/challenge_01.php">Bike Challenge</a></li>
+          <li><a href="bird-challenge/index.php">Bird Challenge</a></li>
+          
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
