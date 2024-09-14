3aeb80821e941bf9ecd05e55f012917f23e91dcb
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-13 23:12:06 -0400

    Create header nav

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Web 250: OOP Home</title>
+  </head>
+
+  <body>
+    <header>
+      <h1>Web 250 OOP: Home Directory</h1>
+      <nav role="navigation">
+        <ul>
+          <li><a href="asgn01/index.php">Asgn01</a></li>
+          <li><a href="asgn02-inheritance/index.php">Asgn02: Inheritance</a></li>
+          <li><a href="asgn03-static/index.php">Asgn03: Static</a></li>
+          <li><a href="asgn04-constructors/index.php">Asgn04: Constructors</a></li>
+          <li><a href="static-challenge/index.php">Static Challenge</a></li>
+          <li><a href="sandbox/index.php">Sandbox</a></li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
