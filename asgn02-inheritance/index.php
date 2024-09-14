784f4214162d06a2be5a9f0ab2e3d014c16df470
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-13 23:19:09 -0400

    Create index for asgn02 directory:

---
+++ asgn02-inheritance/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Web 250: Asgn02 Home</title>
+  </head>
+
+  <body>
+    <header>
+      <h1>Asgn 02: Inheritance</h1>
+      <nav role="navigation">
+        <ul>
+          <li><a href="challenge-02-inheritance.php">Chapter 2 Inheritance Challenge</a></li>
+          <li><a href="challenge-03-oac.php">Chapter 3 Object Access Challenge</a></li>
+          <li><a href="inheritance.php">Inheritance</a></li>
+
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
