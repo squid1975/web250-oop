7889cbae30e297db453e3d2ced7e2b56e249d4b8
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-13 23:22:24 -0400

    Create index for asgn04

---
+++ asgn04-constructors/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Web 250: Asgn04 Constructors</title>
+  </head>
+
+  <body>
+    <header>
+      <h1>Web 250 OOP: Home Directory</h1>
+      <nav role="navigation">
+        <ul>
+          <li><a href="homepagebutnot.php">Homepage (but not actually the homepage)</a></li>
+          <li><a href="constructor-arguments.php">Constructor arguments</a></li>
+          <li><a href="constructor.php">Constructor</a></li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
