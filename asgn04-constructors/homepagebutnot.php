8f3944a3e5a584034b14a42a9cbc32e0e45aba8a
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-16 14:52:25 -0400

    Fix autoload.php homepage

--- asgn04-constructors/homepagebutnot.php
+++ asgn04-constructors/homepagebutnot.php
@@ -1,6 +1,19 @@
-<?php include('autoload.php');
+<?php include('autoload.php'); ?>
 
-$acadianFlycatcher = new Bird;
-$acadianFlycatcher->commonName = 'Acadian Flycatcher';
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Autoload.php</title>
+  </head>
+  
+  <body>
+    <?php 
+    $acadianFlycatcher = new Bird;
+    $acadianFlycatcher->commonName = 'Acadian Flycatcher';
+    
+    echo "Common Name:" . $acadianFlycatcher->commonName . "<br>";
+    ?>
+  </body>
+</html>
 
-echo "Common Name:" . $acadianFlycatcher->commonName . "<br>";
