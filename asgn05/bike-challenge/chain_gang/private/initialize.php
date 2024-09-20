a9cd51795ce0f9a51cb607c64ab9c8a13270a441
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-20 15:02:00 -0400

    Add autoload function to initialize.php

--- asgn05/bike-challenge/chain_gang/private/initialize.php
+++ asgn05/bike-challenge/chain_gang/private/initialize.php
@@ -27,5 +27,11 @@
   // Load class definitions manually
 
   // Autoload class definitions
-
+  function my_autoload($class) {
+    $class = strtolower($class);
+    if(preg_match('/\A\w+\Z/', $class)){
+      include('classes/' . $class . 'class.php');
+    }
+  }
+  spl_autoload_register('my_autoload');
 ?>
