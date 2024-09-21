913443c9b3ddc5d7d222910bcba5d7d3ce22b5d1
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-20 22:54:33 -0400

    Put table to display in birds.php

--- asgn05/bird-challenge/public/birds.php
+++ asgn05/bird-challenge/public/birds.php
@@ -11,28 +11,34 @@
   Create a table. The header should reflect the headings in the wnc-birds.csv class.
   Use a table border of 1 to make the display easier to read.
 */
-   
-<?php
 
-  $parser = new ParseCSV(PRIVATE_PATH . '/wnc-birds.csv');
-  $bird_array = $parser->parse();
+<table>
+      <tr>
+        <th>Common Name</th>
+        <th>Habitat</th>
+        <th>Food</th>
+        <th>Nest Placement</th>
+        <th>Behavior</th>
+        <th>Conservation ID</th>
+        <th>Backyard Tips</th>
+      </tr>
 
-/*
-  Create a foreach array using $bird_array as $args
-  Create a new instance of $bird
-*/
-
-  
-  
-/*
-  Create a table row that lists out all of the bird
-  properties.
-
-*/
-  
-  ?>
-      
-      <?php } ?>
+    <?php 
+    $parser = new ParseCSV(PRIVATE_PATH . '/wnc-birds.csv');
+    $bird_array = $parser->parse();
+    ?>
+     <?php foreach($bird_array as $arguments) { ?>
+      <?php $bird = new Bird($arguments); ?>
+      <tr>
+        <td><?php echo h($bird->commonName);?></td>
+        <td><?php echo h($bird->habitat);?></td>
+        <td><?php echo h($bird->food);?></td>
+        <td><?php echo h($bird->nestPlacement);?></td>
+        <td><?php echo h($bird->behavior);?></td>
+        <td><?php echo h($bird->backyardTips);?></td>
+        <td><?php echo h($bird->conservationID);?></td>
+      </tr>
+    <?php } ?>
 
     </table>
 
