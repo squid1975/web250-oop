130481466d84aebb326b742e2e39fdc29522b2e4
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-20 22:31:47 -0400

    Change autoload function to work with strlower() and .class names

--- asgn05/bird-challenge/private/initialize.php
+++ asgn05/bird-challenge/private/initialize.php
@@ -23,8 +23,9 @@
 
   // Autoload class definitions
   function my_autoload($class) {
-    if(preg_match('/\A\w+\Z/', $class)) {
-      include('classes/' . $class . '.class.php');
+    $class = strtolower($class);
+    if(preg_match('/\A\w+\Z/', $class)){
+      include('classes/' . $class . '.' . 'class.php');
     }
   }
   spl_autoload_register('my_autoload');
