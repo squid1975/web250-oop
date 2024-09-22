9363c60b998505230c00905b17bc681ab5e1278d
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-09-21 20:06:44 -0400

    Add index page for bike/bird challenge

---
+++ asgn05/index.php
@@ -0,0 +1,23 @@
+!,
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Asgn05: Bike, Bird Challenge</title>
+</head>
+
+<body>
+  <header>
+    <h1>Asgn 05: Bike, Bird Challenge Home</h1>
+    <nav>
+      <ul>
+        <li><a href="bike-challenge/chain_gang/public/index.php">Bike Challenge</li>
+        <li><a href="bird-challenge/public/index.php">Bird Challenge</li>
+      </ul>
+    </nav>
+  </header>
+</body>
+
+</html>
